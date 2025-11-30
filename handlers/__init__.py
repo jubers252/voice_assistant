59daf16f7ec593fe3c3b365402487bf8e996dda2
jubers252 <sarwadjuber378@gmail.com>
2025-11-30 19:16:49 +0530

    changes updated

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,10 +5,9 @@ Contains specialized handlers for different aspects of the voice assistant
 
 from .tool_action_handler import ToolActionHandler
 from .wake_word_manager import WakeWordManager
-from .command_processor import CommandProcessor
+
 
 __all__ = [
     'ToolActionHandler',
     'WakeWordManager',
-    'CommandProcessor'
 ]
